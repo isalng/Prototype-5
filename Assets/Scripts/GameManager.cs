2a63f51221ad51ec273a9cc4b09cb3d3a951c987
isalng <122053624+isalng@users.noreply.github.com>
2023-04-10 11:59:20 -0700

    P, 5.3.6

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -47,5 +48,10 @@ public class GameManager : MonoBehaviour
         {
             gameOverText.gameObject.SetActive(true);
             isGameActive = false;
+        }
+
+        public void RestartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 }
